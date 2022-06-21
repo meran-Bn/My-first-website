b761687cc25883c98bb30453036290a2bdedf3e9
meran-Bn <73915094+meran-Bn@users.noreply.github.com>
2022-06-21 20:31:13 +0200

    Vegitarian foods

---
+++ assignment.js
@@ -0,0 +1,34 @@
+
+let myNav = document.getElementById("my-nav");
+function openNav() {
+myNav.style.display = "block";
+myNav.style.height = "70%";
+}
+function closeNav() {
+ myNav.style.display = "none";
+ myNav.style.height = "0%";
+ }
+ 
+
+ 
+let searchNav = document.getElementById("search-nav");
+function openSearch() {
+ searchNav.style.display = "block";
+ searchNav.style.height = "10%";
+}
+function closeSearch() {
+ searchNav.style.display = "none";
+ searchNav.style.height = "0%";
+ }
+
+ let loginEl = document.getElementById("login-el");
+ function openLogin() {
+loginEl.style.display = "block"
+loginEl.style.height = "50%";
+ }
+
+ function closelogin() {
+  loginEl.style.display = "none"
+loginEl.style.height = "0%";
+ }
+
